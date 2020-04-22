4b37808684407fad692cb68fe8f297df12c9fd5d
minhvan251 <minhvan251@gmail.com>
2020-04-22 15:18:12 -0400

    second commit

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,17 @@
-from django.db import models
-
-# Create your models here.
+# from django.db import models
+# from django.contrib.auth import get_user_model
+# from django.contrib.auth.models import (
+#                                         AbstractBaseUser,
+#                                         BaseUserManager,
+#                                         PermissionMixin
+# )
+# # User = get_user_model
+# class UserManager(BaseUserManager):
+#     def create_user(self,email,password=None,**kw):
+#         user=self.model(email=email, **kw)
+#         user.set_password(password)
+#         user.save()
+#         return user
+# # Create your models here.
+# class User(AbstractBaseUser,PermissionMixin):
+# 
