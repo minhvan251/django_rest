579e4a5c2325053d77d3bf6355498a036feb5eeb
minhvan251 <minhvan251@gmail.com>
2020-04-22 19:44:35 -0400

    fix flake8

--- app/core/models.py
+++ app/core/models.py
@@ -1,19 +1,22 @@
 from django.db import models
-from django.contrib.auth import get_user_model
 from django.contrib.auth.models import (
                                         AbstractBaseUser,
                                         BaseUserManager,
                                         PermissionsMixin,
 )
 # User = get_user_model
+
+
 class UserManager(BaseUserManager):
-    def create_user(self,email,password=None,**kw):
-        user=self.model(email=email, **kw)
+    def create_user(self, email, password=None, **kw):
+        user = self.model(email=email, **kw)
         user.set_password(password)
         user.save()
         return user
 # Create your models here.
-class User(AbstractBaseUser,PermissionsMixin):
+
+
+class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
