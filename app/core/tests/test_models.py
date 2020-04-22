c2fe78fbb9f8614b566ffc292df3c096bb828faf
minhvan251 <minhvan251@gmail.com>
2020-04-22 19:47:10 -0400

    fix flake8

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,14 +1,15 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+
 class Modetest(TestCase):
     def test_create_user_with_email(self):
         """Test creating a new user with an email if successful """
         email = 'test@test.com'
-        password ='TestPass0244'
+        password = 'TestPass0244'
         user = get_user_model().object.create_user(
             email=email,
             password=password
         )
-        self.assertEqual(user.email,email)
+        self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
