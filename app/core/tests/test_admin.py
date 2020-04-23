48b32458fae6db80cb287d87e308e1c550357b13
minhvan251 <minhvan251@gmail.com>
2020-04-22 22:16:36 -0400

    user & admin test

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -27,6 +27,7 @@ class AdminSiteTests(TestCase):
 
     def test_user_create_page(self):
         self.setup()
+        print('tset')
         url = reverse('admin:core_user_add')
         res = self.client.get(url)
         self.assertEqual(res.status_code, 200)
